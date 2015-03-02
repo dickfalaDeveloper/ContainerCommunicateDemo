ecc6533d13bd5f8094563ccdb1a969780a04cb72
James <yuanyu.chang@coretronic.com>
2015-03-02 14:21:02 +0800

    Initial Commit

---
+++ ContainerCommunication2Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ContainerCommunication2Demo
+//
+//  Created by James on 2015/3/2.
+//  Copyright (c) 2015年 yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
